d73e09d37177c50b13963b109d9991cc3f6868e9
CoderSwarup03 <swarupmahata321@gmail.com>
2025-10-29 12:01:23 +0530

    setup reactjs frist app

---
+++ reactjs-day-1/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div className='flex flex-col items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-green-500 p-4 rounded-md'>
+        <span className='text-xl text-center text-gray-500'>Welcome to React Day1</span>
+        <h1 className='text-3xl font-bold'>Frist React App</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
